4ad62bab89561955d84bc98e015718e6201ce9ae
taoxiao <taoxiao@zhilehuo.onaliyun.com>
2026-05-09 19:29:58 +0800

    fix

--- chat-client-main/src/renderer/src/constant/api.js
+++ chat-client-main/src/renderer/src/constant/api.js
@@ -1,9 +1,9 @@
 export const api = {
 	prot: "6742",
 	prodDomain: " http://8.136.30.123:7777",
-	// devDomain: " http://106.53.223.225:7777",
-	devDomain: "http://127.0.0.1:7777",
 	prodWsDomain: "ws://8.136.30.123:18023/ws",
-	// devWsDomain: "ws://106.53.223.225:18023/ws",
+
+	devDomain: "http://127.0.0.1:7777",
 	devWsDomain: "ws://127.0.0.1:18023/ws"
 };
+
